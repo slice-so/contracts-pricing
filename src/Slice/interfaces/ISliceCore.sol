45157b54b07912e39b5da25eb295fdd1becb4c20
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-09-04 21:25:31 +0200

    remove unneeded interfaces

--- src/Slice/interfaces/ISliceCore.sol
+++ src/Slice/interfaces/ISliceCore.sol
@@ -2,77 +2,76 @@
 pragma solidity ^0.8.0;
 
 import "../structs/SliceParams.sol";
-import "./utils/IOwnable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC2981Upgradeable.sol";
 
-interface ISliceCore is IOwnable, IERC1155Upgradeable, IERC2981Upgradeable {
-    function slice(SliceParams calldata params) external;
+interface ISliceCore is IERC1155Upgradeable, IERC2981Upgradeable {
+  function slice(SliceParams calldata params) external;
 
-    function reslice(
-        uint256 tokenId,
-        address payable[] calldata accounts,
-        int32[] calldata tokensDiffs
-    ) external;
+  function reslice(
+    uint256 tokenId,
+    address payable[] calldata accounts,
+    int32[] calldata tokensDiffs
+  ) external;
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) external override;
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 id,
+    uint256 amount,
+    bytes memory data
+  ) external override;
 
-    function safeBatchTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) external override;
+  function safeBatchTransferFrom(
+    address from,
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) external override;
 
-    function slicerBatchTransfer(
-        address from,
-        address[] memory recipients,
-        uint256 id,
-        uint256[] memory amounts,
-        bool release
-    ) external;
+  function slicerBatchTransfer(
+    address from,
+    address[] memory recipients,
+    uint256 id,
+    uint256[] memory amounts,
+    bool release
+  ) external;
 
-    function safeTransferFromUnreleased(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) external;
+  function safeTransferFromUnreleased(
+    address from,
+    address to,
+    uint256 id,
+    uint256 amount,
+    bytes memory data
+  ) external;
 
-    function setController(uint256 id, address newController) external;
+  function setController(uint256 id, address newController) external;
 
-    function setRoyalty(
-        uint256 tokenId,
-        bool isSlicer,
-        bool isActive,
-        uint256 royaltyPercentage
-    ) external;
+  function setRoyalty(
+    uint256 tokenId,
+    bool isSlicer,
+    bool isActive,
+    uint256 royaltyPercentage
+  ) external;
 
-    function royaltyInfo(uint256 tokenId, uint256 salePrice)
-        external
-        view
-        override
-        returns (address receiver, uint256 royaltyAmount);
+  function royaltyInfo(uint256 tokenId, uint256 salePrice)
+    external
+    view
+    override
+    returns (address receiver, uint256 royaltyAmount);
 
-    function slicers(uint256 id) external view returns (address);
+  function slicers(uint256 id) external view returns (address);
 
-    function controller(uint256 id) external view returns (address);
+  function controller(uint256 id) external view returns (address);
 
-    function totalSupply(uint256 id) external view returns (uint256);
+  function totalSupply(uint256 id) external view returns (uint256);
 
-    function supply() external view returns (uint256);
+  function supply() external view returns (uint256);
 
-    function exists(uint256 id) external view returns (bool);
+  function exists(uint256 id) external view returns (bool);
 
-    function _setBasePath(string calldata basePath_) external;
+  function _setBasePath(string calldata basePath_) external;
 
-    function _togglePause() external;
+  function _togglePause() external;
 }
--- src/Slice/interfaces/extensions/Purchasable/ISlicerPurchasablePayable.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
-
-import "./ISlicerPurchasable.sol";
-
-interface ISlicerPurchasablePayable is ISlicerPurchasable {
-    /// @notice Releases the ETH balance of this contract to the specified `_collector`.
-    function releaseToCollector() external;
-}
