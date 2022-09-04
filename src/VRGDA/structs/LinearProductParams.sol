d65bd20db7eccc90fde2db3cd9836ef9ec107eb1
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-09-04 04:00:53 +0200

    renamed LinearVRGDA structs

--- src/VRGDA/structs/LinearProductParams.sol
+++ src/VRGDA/structs/LinearProductParams.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.0;
 
 import { LinearVRGDAParams } from "./LinearVRGDAParams.sol";
 
+/// @param startTime Time when the VRGDA began.
 /// @param startingUnits Units available at the time when product is set up.
 /// @param decayConstant Precomputed constant that allows us to rewrite a pow() as an exp().
 /// @param pricingParams See `LinearVRGDAParams`
-struct ProductParams {
+struct LinearProductParams {
   uint256 startTime;
   uint256 startUnits;
   int256 decayConstant;
