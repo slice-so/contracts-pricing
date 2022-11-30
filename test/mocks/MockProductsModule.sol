0a85bdbcda895ac667f48a7c2187f91ff0995b24
Dom-Mac <domenicochad@gmail.com>
2022-11-30 16:31:31 +0100

    added prettier

--- test/mocks/MockProductsModule.sol
+++ test/mocks/MockProductsModule.sol
@@ -2,19 +2,19 @@
 pragma solidity >=0.8.0;
 
 contract MockProductsModule {
-    function isProductOwner(
-        uint256,
-        uint256,
-        address
-    ) external pure returns (bool isAllowed) {
-        isAllowed = true;
-    }
+  function isProductOwner(
+    uint256,
+    uint256,
+    address
+  ) external pure returns (bool isAllowed) {
+    isAllowed = true;
+  }
 
-    function availableUnits(
-        uint256,
-        uint256
-    ) external pure returns (uint256 units, bool isInfinite) {
-        units = 6392;
-        isInfinite = false;
-    }
+  function availableUnits(
+    uint256,
+    uint256
+  ) external pure returns (uint256 units, bool isInfinite) {
+    units = 6392;
+    isInfinite = false;
+  }
 }
