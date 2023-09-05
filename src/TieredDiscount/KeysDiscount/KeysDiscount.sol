8a11e4f72ddfc3a0923acf7cdd629e23d70a78d7
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-09-05 10:50:58 +0200

    feat: KeysDiscount

---
+++ src/TieredDiscount/KeysDiscount/KeysDiscount.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.19;
+
+import {CurrencyParams} from "./structs/CurrencyParams.sol";
+import {DiscountParams, ProductDiscounts, DiscountType, ERC721Discount} from "../ERC721Discount/ERC721Discount.sol";
+import {IFriendTechShares} from "./interfaces/IFriendTechShares.sol";
+
+/**
+ * @title   KeysDiscount - Slice pricing strategy with discounts based on Friend Tech keys ownership
+ * @author  Dom-Mac <@zerohex_eth>
+ * @author  jacopo <@jj_ranalli>
+ */
+
+contract KeysDiscount is ERC721Discount {
+    /*//////////////////////////////////////////////////////////////
+                           IMMUTABLE STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    IFriendTechShares public constant friendTechShares = IFriendTechShares(0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4);
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(address _productsModuleAddress) ERC721Discount(_productsModuleAddress) {}
+
+    /*//////////////////////////////////////////////////////////////
+                                INTERNAL
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Gets the highest discount available for a user, based on owned keys.
+     *
+     * @param discountParams `ProductDiscounts` struct
+     * @param buyer Address of the buyer
+     *
+     * @return Discount value
+     */
+    function _getHighestDiscount(ProductDiscounts memory discountParams, address buyer)
+        internal
+        view
+        override
+        returns (uint256)
+    {
+        DiscountParams[] memory discounts = discountParams.discountsArray;
+        uint256 length = discounts.length;
+        DiscountParams memory el;
+
+        address prevAsset;
+        uint256 keysBalance;
+        for (uint256 i; i < length;) {
+            el = discounts[i];
+
+            // Skip retrieving balance if asset is the same as previous iteration
+            if (prevAsset != el.asset) {
+                keysBalance = friendTechShares.sharesBalance(el.asset, buyer);
+            }
+
+            // Check if user has at enough keys to qualify for the discount
+            if (keysBalance >= el.minQuantity) {
+                return el.discount;
+            }
+
+            prevAsset = el.asset;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        // Otherwise default to no discount.
+        return 0;
+    }
+}
---
+++ src/TieredDiscount/KeysDiscount/structs/CurrencyParams.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {DiscountParams} from "../../structs/DiscountParams.sol";
+import {DiscountType} from "../../structs/ProductDiscounts.sol";
+
+/// @param currency             currency address
+/// @param basePrice            base price for a currency
+/// @param isFree               boolean flag that allows purchase when basePrice == 0`
+/// @param discountType         type of discount, can be `Absolute` or `Relative`
+/// @param discounts            array of DiscountParams
+struct CurrencyParams {
+    address currency;
+    uint240 basePrice;
+    bool isFree;
+    DiscountType discountType;
+    DiscountParams[] discounts;
+}
