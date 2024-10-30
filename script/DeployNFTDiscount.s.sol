cd16c5126a2a99ce0c8c56b6d364aead521972b4
defijesus.eth <defijesus@duck.com>
2024-10-30 18:42:51 +0000

    add new ScriptUtils

--- script/DeployNFTDiscount.s.sol
+++ script/DeployNFTDiscount.s.sol
@@ -1,25 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8;
 
-import "forge-std/Script.sol";
+import {EthereumScript, BaseScript} from "./utils/ScriptUtils.sol";
 import {NFTDiscount} from "../src/TieredDiscount/NFTDiscount/NFTDiscount.sol";
 
-contract DeployScript is Script {
-    function run() external {
 
-        // address productsModule = 0x689Bba0e25c259b205ECe8e6152Ee1eAcF307f5F; // mainnet
-        // address productsModule = 0xcA6b9D59849EC880e82210e9cb8237E1d0cAA69e; // goerli testnet
-        // address productsModule = 0x0FD0d9aa44a05Ee158DDf6F01d7dcF503388781d; // goerli staging
-        // address productsModule = 0x0FD0d9aa44a05Ee158DDf6F01d7dcF503388781d; // base goerli
-        address productsModule = 0xb9d5B99d5D0fA04dD7eb2b0CD7753317C2ea1a84; // base
-        // address productsModule = 0x61bCd1ED11fC03C958A847A6687b1875f5eAcaaf; // optimism
+/// make deploy-ledger contract=script/DeployNFTDiscount.s.sol:DeployEthereum chain=mainnet
 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+contract DeployEthereum is EthereumScript {
+    function run() external broadcast {
+        new NFTDiscount(PRODUCTS_MODULE);
+    }
+}
 
-        vm.startBroadcast(deployerPrivateKey);
+/// make deploy-ledger contract=script/DeployNFTDiscount.s.sol:DeployBase chain=base
 
-        new NFTDiscount(productsModule);
-
-        vm.stopBroadcast();
+contract DeployBase is BaseScript {
+    function run() external broadcast {
+        new NFTDiscount(PRODUCTS_MODULE);
     }
 }
